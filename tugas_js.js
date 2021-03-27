21c466e3212230158abbff0e52f159763bc6b6d8
annis <nissinkentang@gmail.com>
2021-03-28 00:57:53 +0700

    first commit

---
+++ tugas_js.js
@@ -0,0 +1,27 @@
+// var today = new Date();
+// var dd = String(today.getDate()).padStart(2, '0');
+// var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+// var yyyy = today.getFullYear();
+
+// today = dd + '/' + mm + '/' + yyyy;
+// document.write(today);
+// var dt = new Date();
+// document.getElementById("datetime").innerHTML = dt.toLocaleDateString();
+// var today = new Date();
+//             var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+//             document.getElementById("tanggal").innerHTML = date;
+let today = new date
+document.getElementById("tanggal").innerHTML = date;
+
+let today = new TimeRanges
+document.getElementById("jm").innerHTML = TimeRanges;
+
+
+
+// Date()
+// let tanggal = new Date()
+// document.getElementById('tgll').innerHTML=tanggal.getDate().getMonth().getFullYear()
+
+// Hours()
+// let jam= Hours()
+// document.getElementById('jm').innerHTML=jam.getHours().getMinutes().getSeconds()
